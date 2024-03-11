db5b500d9d1e10165fdb2dbbcfba906422e74adc
canjey <carolnjeri3235@gmail.com>
2024-03-11 06:11:47 +0300

    ref/changed fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import logo from './logo.svg';
+import { Typography } from '@mui/material';
 import './App.css';
+import LandingPage from './Components/LandingPage';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Features from './Components/LandingPage/Features';
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary:{
+      // light:'#fffff',
+      main:'#FFCC00',
+    },
+    typography: {
+      p: {
+        fontSize: "4rem",
+        fontWeight: 700,
+      },
+    },
+   
+
+  },
+});
 
 function App() {
   return (
+    <ThemeProvider theme={darkTheme}>
+
     <div className="App">
-      Test
+      <Features />
+      <Typography variant='p'>
+        Hi My name is Carol
+      </Typography>
     </div>
+    </ThemeProvider>
+
   );
 }
 
---
+++ src/Components/LandingPage/Features.js
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+export default function Features(){
+    return(
+        <>
+        <Typography variant="p">
+            Hi My name is Carol
+        </Typography>
+        </>
+    )
+}
